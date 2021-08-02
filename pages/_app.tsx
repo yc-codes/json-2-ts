6acfe95e8c17651c1691019ab961d8b385b2719c
Yash Chauhan <yc9274@gmail.com>
2021-08-02 18:11:07 +0530

    added: meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="google-site-verification" content="g3DXYh4M3WRdin5hSE4hR5Gf-ZT0yJd_hiFiTF3M674" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 export default MyApp
