ca7872b0de54aac0ade2a5ac2a28335dbc9ae16a
Yash Chauhan <yc9274@gmail.com>
2021-08-03 14:11:20 +0530

    added: google verification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,6 @@ import Head from "next/head"
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-      <meta name="google-site-verification" content="g3DXYh4M3WRdin5hSE4hR5Gf-ZT0yJd_hiFiTF3M674" />
     </Head>
     <Component {...pageProps} />
   </>
